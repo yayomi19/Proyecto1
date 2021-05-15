f54d1aafd4b70dd4dd24cacacc79a6f29fc44c05
yayomi19 <81947414+yayomi19@users.noreply.github.com>
2021-05-14 19:24:03 -0500

    subiendo ejemplo 5

---
+++ ejemplo5.py
@@ -0,0 +1,13 @@
+def celsius_a_fahrenheit(temp_c):
+    '''
+    transforma temperatura de celsius a fahrenheit
+    Input: 
+        temp_c:número float
+    Ouput: 
+        retorna: número float
+    '''
+    
+    temp_f = 1.8*temp_c + 32
+    
+    return temp_f 
+    
